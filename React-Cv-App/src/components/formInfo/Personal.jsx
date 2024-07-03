d770345e4ba7a72d8e3ac44c3c192811e37a943f
alanzhang03 <alan.s.zhang@gmail.com>
2024-07-03 16:42:16 -0400

    basic setup

--- React-Cv-App/src/components/formInfo/Personal.jsx
+++ React-Cv-App/src/components/formInfo/Personal.jsx
@@ -3,11 +3,22 @@ import "../componentStyles/FormComp.css";
 export default function Personal() {
 	return (
 		<div className="personal">
-			<form>
-                <h1>Personal Details</h1>
-                
-                <label for="fullName">Full Name</label>
-                <input type="text" id="fullName"></input>
+			<form className="personal-details-form">
+				<h1>Personal Details</h1>
+
+				<label for="fullName">Full Name</label>			
+				<input type="text" id="fullName" name="fullName"></input>
+			
+
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email"></input>		
+
+				<label for="phone">Phone Number</label>
+				<input type="tel" id="phone" name="phone"></input>
+		
+
+				<label for="address">Address</label>
+				<input type="text"></input>
 			</form>
 		</div>
 	);
