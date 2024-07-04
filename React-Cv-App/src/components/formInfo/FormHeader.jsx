53378d96af06dd616160ee0de41830c5663ecc89
alanzhang03 <alan.s.zhang@gmail.com>
2024-07-04 13:44:00 -0400

    form header

--- React-Cv-App/src/components/formInfo/FormHeader.jsx
+++ React-Cv-App/src/components/formInfo/FormHeader.jsx
@@ -3,7 +3,19 @@ import "../componentStyles/FormHeader.css";
 export default function FormHeader() {
 	return (
 		<div className="form-header">
-			<h1>Form header!</h1>
+			<div className="form-header-container">
+				<div className="clear-resume">
+					<button className="clear-resume-button">
+						🗑️Clear Resume
+					</button>
+				</div>
+				<div className="load-example">
+					<button className="load-example-button">
+						Load Example
+					</button>
+				</div>
+			</div>
+
 		</div>
 	);
 }
