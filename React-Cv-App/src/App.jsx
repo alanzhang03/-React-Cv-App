3545f362f171d1d09151ebde27ef9f6e47b633b6
alanzhang03 <alan.s.zhang@gmail.com>
2024-07-07 13:59:45 -0400

    resume container

--- React-Cv-App/src/App.jsx
+++ React-Cv-App/src/App.jsx
@@ -1,5 +1,4 @@
 import Footer from "./components/Footer";
-import Form from "./components/formInfo/Form";
 import "./assets/styles/App.css";
 import ResumeBuilder from "./components/resume/ResumeBuilder";
 
--- React-Cv-App/src/components/resume/ResumePreview.jsx
+++ React-Cv-App/src/components/resume/ResumePreview.jsx
@@ -4,14 +4,22 @@ import "../componentStyles/ResumePreview.css";
 export default function ResumePreview({ data }) {
 	return (
 		<div className="resume-preview">
+
+
 			<div className="resume-personal-container">
 				<h1 className="full-name">{data.fullName}</h1>
-				<div className="info-container">
+				<div className="personal-info-container">
 					<li>✉️ {data.email}</li>
 					<li>📞 {data.phone}</li>
 					<li>📍 {data.address}</li>
 				</div>
 			</div>
+			<div className="resume-other-container">
+
+				
+			</div>
+
+
 		</div>
 	);
 }
